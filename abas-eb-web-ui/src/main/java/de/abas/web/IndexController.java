bd4adf242f3c67f4a49e8a04e94f13ebd6db2880
Vincent Potucek <vincent.potucek@abas.de>
2018-05-29 10:19:05 +0200

    first draw of page layout including header

--- abas-eb-web-ui/src/main/java/de/abas/web/IndexController.java
+++ abas-eb-web-ui/src/main/java/de/abas/web/IndexController.java
@@ -2,8 +2,6 @@ package de.abas.web;
 
 import de.abas.service.DataService;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,20 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
-@Slf4j
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("web")
-public class WelcomeController {
+public class IndexController {
 
     @Autowired
     private final DataService.Factory factory;
 
     @GetMapping
-    public String example(Map<String, Object> model) {
+    public String index(Map<String, Object> model) {
         String foo22 = factory.create("42").foo22();
-        log.info("example request with foo22: '{}'", foo22);
         model.put("message", foo22);
-        return "example";
+        return "index";
     }
 }
