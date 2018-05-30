90eefb2204eb524b700069b0ae259be845eb2ffc
Vincent Potucek <vincent.potucek@abas.de>
2018-05-30 09:51:58 +0200

    format code

--- abas-eb-web-core/src/main/java/de/abas/service/DataServiceImpl.java
+++ abas-eb-web-core/src/main/java/de/abas/service/DataServiceImpl.java
@@ -9,30 +9,30 @@ import java.util.Map;
 @Value
 class DataServiceImpl implements DataService {
 
-	String criteria;
+    String criteria;
 
-	public Collection<Item> items() {
-		return List.of(
-				new Item(Map.of(
-						"foo", criteria + " - " + System.currentTimeMillis(),
-						"bar", criteria + " - " + System.currentTimeMillis(),
-						"baz", criteria + " - " + System.currentTimeMillis())),
-				new Item(Map.of(
-						"foo", criteria + " - " + System.currentTimeMillis(),
-						"bar", criteria + " - " + System.currentTimeMillis(),
-						"baz", criteria + " - " + System.currentTimeMillis())),
-				new Item(Map.of(
-						"foo", criteria + " - " + System.currentTimeMillis(),
-						"bar", criteria + " - " + System.currentTimeMillis(),
-						"baz", criteria + " - " + System.currentTimeMillis())),
-				new Item(Map.of(
-						"foo", criteria + " - " + System.currentTimeMillis(),
-						"bar", criteria + " - " + System.currentTimeMillis(),
-						"baz", criteria + " - " + System.currentTimeMillis())),
-				new Item(Map.of(
-						"foo", criteria + " - " + System.currentTimeMillis(),
-						"bar", criteria + " - " + System.currentTimeMillis(),
-						"baz", criteria + " - " + System.currentTimeMillis())));
-	}
+    public Collection<Item> items() {
+        return List.of(
+                new Item(Map.of(
+                        "foo", criteria + " - " + System.currentTimeMillis(),
+                        "bar", criteria + " - " + System.currentTimeMillis(),
+                        "baz", criteria + " - " + System.currentTimeMillis())),
+                new Item(Map.of(
+                        "foo", criteria + " - " + System.currentTimeMillis(),
+                        "bar", criteria + " - " + System.currentTimeMillis(),
+                        "baz", criteria + " - " + System.currentTimeMillis())),
+                new Item(Map.of(
+                        "foo", criteria + " - " + System.currentTimeMillis(),
+                        "bar", criteria + " - " + System.currentTimeMillis(),
+                        "baz", criteria + " - " + System.currentTimeMillis())),
+                new Item(Map.of(
+                        "foo", criteria + " - " + System.currentTimeMillis(),
+                        "bar", criteria + " - " + System.currentTimeMillis(),
+                        "baz", criteria + " - " + System.currentTimeMillis())),
+                new Item(Map.of(
+                        "foo", criteria + " - " + System.currentTimeMillis(),
+                        "bar", criteria + " - " + System.currentTimeMillis(),
+                        "baz", criteria + " - " + System.currentTimeMillis())));
+    }
 
 }
